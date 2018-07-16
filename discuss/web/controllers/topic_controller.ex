6e724f33e497bc66990d571be7ee1b0077c560f8
sebaughman <sarahbaughman@u.boisestate.edu>
2018-07-16 14:34:04 -0600

    view all topics

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -2,6 +2,7 @@ defmodule Discuss.TopicController do
     use Discuss.Web, :controller
 
     alias Discuss.Topic
+
     def new(conn, _params) do
         struct = %Topic{}
         params = %{}
@@ -12,7 +13,20 @@ defmodule Discuss.TopicController do
 
     def create(conn, params) do
         %{"topic" => topic} = params
+        changeset = Topic.changeset(%Topic{}, topic)
+        case Repo.insert(changeset) do
+            {:ok, post} -> 
+                conn
+                |> put_flash(:info, "Topic Created")
+                |> redirect(to: topic_path(conn, :index))
+            {:error, changeset} -> 
+                render(conn, "new.html", changeset: changeset)
+        end
+    end
 
+    def index(conn, _params) do
+        topics = Repo.all(Topic)
+        render(conn, "index.html", topics: topics)
     end
 end
 
