1ce80c315565f34aaa2f7a98b543835e44d76493
sebaughman <sarahbaughman@u.boisestate.edu>
2018-07-16 16:51:37 -0600

    edit and delete topics

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -15,7 +15,7 @@ defmodule Discuss.TopicController do
         %{"topic" => topic} = params
         changeset = Topic.changeset(%Topic{}, topic)
         case Repo.insert(changeset) do
-            {:ok, post} -> 
+            {:ok, _topic} -> 
                 conn
                 |> put_flash(:info, "Topic Created")
                 |> redirect(to: topic_path(conn, :index))
@@ -27,6 +27,40 @@ defmodule Discuss.TopicController do
     def index(conn, _params) do
         topics = Repo.all(Topic)
         render(conn, "index.html", topics: topics)
+    end
+
+    def edit(conn, params) do 
+        %{"id" => topic_id} = params
+
+        topic = Repo.get(Topic, topic_id)
+        changeset = Topic.changeset(topic)
+
+        render(conn, "edit.html", changeset: changeset, topic: topic)
+    end
+
+    def update(conn, params) do
+        %{"topic" => topic, "id" => topic_id} = params
+        old_topic = Repo.get(Topic, topic_id) 
+        changeset = Topic.changeset(old_topic, topic)
+       
+        case Repo.update(changeset) do
+            {:ok, topic} -> 
+                conn
+                |> put_flash(:info, "Topic Updated")
+                |> redirect(to: topic_path(conn, :index))
+            {:error, changeset} -> 
+                render(conn, "edit.html", changeset: changeset, topic: old_topic)
+        end
+    end
+
+    def delete(conn, params) do
+        %{"id" => topic_id} = params
+        
+        Repo.get!(Topic, topic_id) |> Repo.delete! 
+
+        conn
+        |> put_flash(:info, "Topic Deleted")
+        |> redirect(to: topic_path(conn, :index))
     end
 end
 
