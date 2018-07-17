1ad817d8934f767fbb4e32126e8ee415eccc5e06
sebaughman <sarahbaughman@u.boisestate.edu>
2018-07-17 13:59:05 -0600

    restrict pages if not authenticated

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -3,6 +3,8 @@ defmodule Discuss.TopicController do
 
     alias Discuss.Topic
 
+    plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+
     def new(conn, _params) do
         struct = %Topic{}
         params = %{}
